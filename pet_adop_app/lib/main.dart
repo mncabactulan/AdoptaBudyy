59f763e14eb1fab18bfd7d7da378624b050ee9ac
mncabactulan <95170733+mncabactulan@users.noreply.github.com>
2023-04-26 21:16:31 +0800

    Adding files

---
+++ pet_adop_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:pet_adop_app/pages/onboard.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const OnBoardPage(),
+    );
+  }
+}
---
+++ pet_adop_app/lib/models/owner_model.dart
@@ -0,0 +1,12 @@
+class Owner {
+  final String name, image;
+
+  Owner({required this.name, required this.image});
+}
+
+List<Owner> owners = [
+  Owner(name: 'Lowelle', image: 'assets/owners/woman-with-cat1.png'),
+  Owner(name: 'Harrim', image: 'assets/owners/harrim.png'),
+  Owner(name: 'Jake', image: 'assets/owners/jake.jpg'),
+  Owner(name: 'John Paul', image: 'assets/owners/berbano.png'),
+];
